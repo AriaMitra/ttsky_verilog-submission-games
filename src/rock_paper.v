198506e2082394aec884bf50eb26e4c85c3a32ce
Aria Mitra <ariamitra99@yahoo.com>
2025-08-27 21:02:10 -0500

    Update rock_paper.v

--- src/rock_paper.v
+++ src/rock_paper.v
@@ -6,29 +6,29 @@ module rock_paper(
 );
 	//00 is rock, 01 is paper, 11 is scissors
 	//win → 00 is u win, 01 is draw, 11 is computer wins
-  reg [1:0] rand;
+  reg [1:0] r;
 	wire [2:0] ran;
-  LFSR_3 v1 (.clk(clk), .reset(reset), .rand(ran));
+	LFSR_3 v1 (.clk(clk), .reset(reset), .randd(ran));
 	always @ (posedge clk) begin
-    rand <= ran[1:0];
+    r <= ran[1:0];
 		if (u == 2'b00) begin
-			if (rand == 0)
+			if (r == 0)
 				win <= 2'b01;
-			else if (rand == 3)
+			else if (r == 3)
 				win <= 2'b00;
 			else 
 				win <=2'b11;
 		end else if(u == 2'b01) begin
-			if(rand == 1)
+			if(r == 1)
 				win <=2'b01;
-			else if(rand == 3)
+			else if(r == 3)
 				win <=2'b11;
 			else 
 				win<=2'b00;
     end else begin
-      if(rand ==3)
+      if(r ==3)
         win<=2'b01;
-      else if (rand == 0)
+      else if (r == 0)
         win<=2'b11;
       else
         win<=2'b00;
